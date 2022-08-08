eede340bcb3926bb110bdfee2734cbc308af330f
Luca <luca.mainardi01@gmail.com>
2022-08-08 18:48:09 +0200

    Moved configuration informations to a json file (config.json). This file provides getters and setters methods to access the informations contained in the json

--- config.py
+++ config.py
@@ -1,13 +1,89 @@
-API_KEY = ""
-SECRET_KEY = ""
+import json
+from textwrap import indent
 
-SYMBOL = "ETH/BUSD"
-POSITION_SIZE = 0.006
 
-NUM_BUY_GRID_LINES = 100
-NUM_SELL_GRID_LINES = 100
+def save_data(data):
+    with open("config.json", "w") as f:
+        json.dump(data, f, indent=4)
 
-GRID_SIZE = 7
 
-CHECK_FREQUENCY = 1
-CLOSED_ORDER_STATUS = 'closed'
+def load_data():
+    try:
+        with open("config.json", "r") as f:
+            data = json.load(f)
+            return data
+    except:
+        return{}
+
+
+PATH = "config.json"
+
+# _________________________________ GETTERS ________________________________
+
+
+def get_Symbol():
+    config = load_data()
+    return config['SYMBOL']
+
+
+def get_Position_Size():
+    config = load_data()
+    return float(config['POSITION_SIZE'])
+
+
+def get_Num_Buy_Grid_Lines():
+    config = load_data()
+    return int(config['NUM_BUY_GRID_LINES'])
+
+
+def get_Num_Sell_Grid_Lines():
+    config = load_data()
+    return int(config['NUM_SELL_GRID_LINES'])
+
+
+def get_Grid_Size():
+    config = load_data()
+    return float(config['GRID_SIZE'])
+
+
+def get_Check_Frequency():
+    config = load_data()
+    return float(config['CHECK_FREQUENCY'])
+
+# _________________________________ SETTERS ________________________________
+
+
+def set_Symbol(new_symbol):
+    config = load_data()
+    config['SYMBOL'] = new_symbol
+    save_data(config)
+
+
+def set_Position_Size(new_position_size):
+    config = load_data()
+    config['POSITION_SIZE'] = new_position_size
+    save_data(config)
+
+
+def set_Num_Buy_Grid_Lines(new_num_buy_grid_lines):
+    config = load_data()
+    config['NUM_BUY_GRID_LINES'] = new_num_buy_grid_lines
+    save_data(config)
+
+
+def set_Num_Sell_Grid_Lines(new_num_sell_grid_lines):
+    config = load_data()
+    config['NUM_SELL_GRID_LINES'] = new_num_sell_grid_lines
+    save_data(config)
+
+
+def set_Grid_Size(new_Grid_Size):
+    config = load_data()
+    config['GRID_SIZE'] = new_Grid_Size
+    save_data(config)
+
+
+def set_Check_Frequency(new_Check_Frequency):
+    config = load_data()
+    config['CHECK_FREQUENCY'] = new_Check_Frequency
+    save_data(config)
