7af0a26be7162421230c4a105849585649f2a27b
Luca <luca.mainardi01@gmail.com>
2022-08-08 18:53:25 +0200

    Now it used the GUI class from GUI.py in order to create the graphic interface

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
-from distutils.command.config import config
-from threading import Thread
 import ccxt
-
 from DemoBot import start_demo_bot, Demo_Account, Order
-from GUI import start_gui
-import matplotlib
-from matplotlib.animation import FuncAnimation
-import matplotlib.pyplot as plt
-from datetime import datetime
+
+from GUI import GUI
 
 
 exchange = ccxt.binance()
 account = Demo_Account(2000)
 
-# buy_orders: List[Order] = []
-# sell_orders: List[Order] = []
-
 
 def main():
 
-    bot = Thread(target=start_demo_bot, args=(
-        exchange, account))
-    bot.start()
-    start_gui(exchange)
+    # bot = Thread(target=start_demo_bot, args=(
+    #     exchange, account))
+    # bot.start()
+    # start_gui(exchange)
+
+    gui = GUI(exchange, account)
+    gui.mainloop()
 
 
 if __name__ == '__main__':
