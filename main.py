c21bc57de94575360a35b5f33d8b01ef576d6f50
Luca <luca.mainardi01@gmail.com>
2022-08-18 17:12:20 +0200

    fixed modules import

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-from tkinter import font
-from tkinter import *
-import ccxt
-from DemoBot import start_demo_bot, Demo_Account
-import config
-
 from GUI import GUI
-import DemoBot
-import multiprocessing
 
 
 def main():
